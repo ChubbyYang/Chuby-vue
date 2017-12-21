b26112092c1b21d98358c9229eaa715e28fd2ae5
ChubbyYang <624468181@qq.com>
2017-12-21 11:12:36 +0800

    communications between components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -20,26 +20,4 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
-=======
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-// 2.0用下面这个
-import 'element-ui/lib/theme-chalk/index.css'
-import App from './App'
-import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
-Vue.use(ElementUI)
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  template: '<App/>',
-  components: { App }
-})
->>>>>>> aa39274aab8a4911f263bf957e2f69102f3dd05f
